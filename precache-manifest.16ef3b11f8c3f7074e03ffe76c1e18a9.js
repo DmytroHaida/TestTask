04fbbb54d78a9c2de43a43fdc8cfd5bffa397547
Pcixo <Pcixo@users.noreply.github.com>
2020-09-26 01:08:49 +0300

    Updates

---
+++ precache-manifest.16ef3b11f8c3f7074e03ffe76c1e18a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5caa91b04e0ffe5b8293a5d3bfb31b73",
+    "url": "/TestTask/index.html"
+  },
+  {
+    "revision": "680604fd77c04eb68a53",
+    "url": "/TestTask/static/css/main.44c1f91c.chunk.css"
+  },
+  {
+    "revision": "3df1774dcefba0632f32",
+    "url": "/TestTask/static/js/2.17ea80ad.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/TestTask/static/js/2.17ea80ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680604fd77c04eb68a53",
+    "url": "/TestTask/static/js/main.1ae3f257.chunk.js"
+  },
+  {
+    "revision": "fd337bbdb3414311cf4c",
+    "url": "/TestTask/static/js/runtime-main.b9979cf6.js"
+  }
+]);
